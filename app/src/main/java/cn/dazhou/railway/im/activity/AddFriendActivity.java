7831c8bb9ee7e4b1dea3315821a5f91802f91a67
hooyee <501206021@qq.com>
2017-05-23 17:59:39 +0800

    + 添加服务器用户拉取的功能(补充提交)

--- app/src/main/java/cn/dazhou/railway/im/activity/AddFriendActivity.java
+++ app/src/main/java/cn/dazhou/railway/im/activity/AddFriendActivity.java
@@ -2,13 +2,17 @@ package cn.dazhou.railway.im.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.dazhou.im.IMLauncher;
 import cn.dazhou.railway.R;
 
 public class AddFriendActivity extends AppCompatActivity {
+    @BindView(R.id.edit_search_user)
+    EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +21,9 @@ public class AddFriendActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.btn_searchfriend)
+    @OnClick(R.id.bt_search_user)
     public void searchUser() {
-        IMLauncher.searchUserFromServer("mao");
+        String username = mEditText.getText().toString();
+        IMLauncher.searchUserFromServer(username);
     }
 }
